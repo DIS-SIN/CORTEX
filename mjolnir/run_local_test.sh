ee5bc7a7bcdf714b65a91712c283cb8fcc701b30
Nghia Doan <hong.nghiadoan@da-an.ca>
2019-08-07 10:39:59 -0400

    Added local yggdrasil and jotunheimr

--- mjolnir/run_local_test.sh
+++ mjolnir/run_local_test.sh
@@ -11,6 +11,8 @@ tar xzvf $DATA_DIR/pc_v2015.tar.gz --directory $DATA_DIR/conv pc_v2015/pccfNat_A
 mv $DATA_DIR/conv/pc_v2015/pccfNat_AUG15_fccpNat.txt $DATA_DIR/conv/pccfNat_AUG15_fccpNat.txt
 rm -rf $DATA_DIR/conv/pc_v2015
 
-./run_yggdrasil.sh
-./run_jotunheimr.sh
+docker-compose up -d yggdrasil_zookeeper yggdrasil_broker yggdrasil_schema_registry yggdrasil_connect yggdrasil_control_center
+
+docker-compose up -d jotunheimr
+
 ./wield_mjolnir.sh -mextsci $DATA_DIR/cp/cp.ini $DATA_DIR/cp $DATA_DIR/tmp $DATA_DIR/cp
