a238e4a71ae374210576a3c9f0e4369d9fac9c8b
Nghia Doan <hong.nghiadoan@da-an.ca>
2019-08-25 00:58:52 -0400

    Added Draupnir.

---
+++ loki/run_draupnir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: ./run_draupnir.sh <yggdrasil_host>"
+  echo "Example: ./run_draupnir.sh 10.0.1.119"
+  exit
+fi
+
+export YGGDRASIL_HOST=$1
+
+python draupnir.py $YGGDRASIL_HOST
